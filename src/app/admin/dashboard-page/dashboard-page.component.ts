d438a0b1333f307b3cd6e14b2f34151462556402
alex_hcj <alexeykorolyov19@gmail.com>
2022-05-04 20:23:52 +0300

    add new design admin dashboard-page

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {PostsService} from "../shared/posts.service";
 import {Post} from "../shared/interfaces";
-import {Subscription} from "rxjs";
+import {Subscription, throwError} from "rxjs";
 import {AlertService} from "../shared/services/alert.service";
 
 @Component({
@@ -9,24 +9,34 @@ import {AlertService} from "../shared/services/alert.service";
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
   posts: Post[] = []
   pSub: Subscription | undefined
   dSub: Subscription | undefined
   searchStr = ''
+  loading = false
 
   constructor(private postsService: PostsService, private alert: AlertService) { }
 
   ngOnInit() {
-    this.postsService.getAll().subscribe(posts => {
-      this.posts = posts
-    })
+    this.loading = true
+    this.pSub = this.postsService.getAll().subscribe({
+      next: posts => {
+        this.posts = posts
+        this.loading = false
+      },
+      error: error => {
+        throwError(error)
+        this.loading = false
+      }
+      }
+    )
   }
 
   remove(id: string) {
     this.dSub = this.postsService.remove(id).subscribe(() => {
       this.posts = this.posts.filter(post => post.id !== id)
-      this.alert.danger('Пост был удалён')
+      this.alert.danger('Post was deleted')
     })
   }
 
