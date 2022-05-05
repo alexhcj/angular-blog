5ae1ba4458234affbf5b8a2c5c84f74b9794a840
alex_hcj <alexeykorolyov19@gmail.com>
2022-05-05 16:52:53 +0300

    add new design & logic create-page

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -11,12 +11,31 @@ import {AlertService} from "../shared/services/alert.service";
 })
 export class CreatePageComponent implements OnInit {
   form!: FormGroup
+  selectCategories = [
+    {category: 'Design'},
+    {category: 'Illustrations'},
+    {category: 'Posters'},
+    {category: 'Interior'},
+    {category: 'Travel'},
+  ]
+  selectTags = [
+    {tag: 'Abstract'},
+    {tag: 'Creative'},
+    {tag: 'Photo'},
+    {tag: 'Art'},
+    {tag: 'Style'},
+    {tag: 'Pattern'},
+    {tag: 'Drawing'},
+  ]
 
   constructor(private postService: PostsService, private alert: AlertService) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
+      imgUrl: new FormControl(null),
       title: new FormControl(null, Validators.required),
+      category: new FormControl(this.selectCategories[0].category, Validators.required),
+      tags: new FormControl(this.selectTags[0].tag, Validators.required),
       text: new FormControl(null, Validators.required),
       author: new FormControl(null, Validators.required),
     })
@@ -27,19 +46,23 @@ export class CreatePageComponent implements OnInit {
       return
     }
 
+    this.form.disable()
+
     const post: Post = {
-      category: this.form.value.category,
+      imgUrl: this.form.value.imgUrl,
       title: this.form.value.title,
-      author: this.form.value.author,
+      category: this.form.value.category,
+      tags: this.form.value.tags,
       text: this.form.value.text,
+      author: this.form.value.author,
       date: new Date(),
-      tags: this.form.value.tags
     }
 
     this.postService.create(post).subscribe({
       next: () => {
         this.form.reset()
-        this.alert.success('Пост был создан')
+        this.alert.success('Post was created')
+        this.form.enable()
       }
     })
   }
